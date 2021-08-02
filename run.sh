58782cf111b0f21fd080345b151c3413f5314f32
Cursky <46676188+Cursky@users.noreply.github.com>
2021-08-02 15:53:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 
 # pip install wechaty
-# pip install dlib -i https://mirror.baidu.com/pypi/simple #人脸检测包暗装
+# pip install dlib -i https://mirror.baidu.com/pypi/simple #人脸检测包
 # git clone https://github.com/PaddlePaddle/PaddleGAN
 # unzip -q data/data76944/PaddleGAN.zip
 # 设置环境变量
 export WECHATY_PUPPET=wechaty-puppet-service
-export WECHATY_PUPPET_SERVICE_TOKEN=puppet_padlocal_9ab56da897ff419bbd9528af987c4532 
+export WECHATY_PUPPET_SERVICE_TOKEN=#此处填写你的token
 
 
 # 运行python文件
